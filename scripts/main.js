c591e1355e68b8ee9ae99c30a6c8482414dd8e63
HelixFix <jerome.rinner@gmail.com>
2020-09-04 17:54:03 +0200

    Return all three elements in a array

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 // alert('yes')
+
+window.onload = () => {
+
+    // const listings1 = document.getElementsByClassName('listings')
+    const listings2 = Array.from(document.getElementsByClassName('listings')) // Même chose que listings1 mais de sorte à retrouné une Array
+
+    console.log(listings);
+    
+}
