715b83469b9d51ad15e443cdcfdc47234dd417b4
HelixFix <jerome.rinner@gmail.com>
2020-09-05 11:27:16 +0200

    Create loop through listings

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,10 @@ window.onload = () => {
     // const listings1 = document.getElementsByClassName('listings')
     const listings2 = Array.from(document.getElementsByClassName('listings')) // Même chose que listings1 mais de sorte à retrouné une Array
 
-    console.log(listings);
+    console.log(listings2);
+
+    // loop through listings
+    listings2.forEach(listing => handleSlider(listing) )
     
 }
+
