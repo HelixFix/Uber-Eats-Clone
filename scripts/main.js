ad1a046d08485eb9314d2ef2b894dcf9154c82db
HelixFix <jerome.rinner@gmail.com>
2020-09-08 16:02:19 +0200

    Add left button slide function

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,35 @@ window.onload = () => {
     
 }
 
+// define function to handle slider
+const handleSlider = listing => {
+    const listingGrid = listing.getElementsByClassName('listings-grid')[0]
+    const arrowLeft = listing.getElementsByClassName('left')[0]
+    const arrowRight = listing.getElementsByClassName('right')[0]
+
+    // Error checking
+
+    if (!listingGrid || !arrowLeft || !arrowRight) {
+        console.log(listing)
+        return
+        
+    }
+
+    // Right Arrow click action
+
+    arrowRight.addEventListener('click', e => {
+        e.preventDefault()
+        listingGrid.scrollTo({
+            left: listingGrid.offsetWidth,
+            behaviour: 'smooth'
+        })
+    })
+    
+    arrowLeft.addEventListener('click', e => {
+        e.preventDefault()
+        listingGrid.scrollTo({
+            left: 0, // Retour point d'origine
+            behaviour: 'smooth'
+        })
+    })
+}
